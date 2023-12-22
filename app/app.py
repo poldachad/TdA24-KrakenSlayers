43ce4a0099a562f3d7e08c28cd6df25077ae28bd
Jan Lysý <jan.lysy06@gmail.com>
2023-12-22 16:13:06 +0100

    uz mame html stranky

--- app/app.py
+++ app/app.py
@@ -1,12 +1,12 @@
 import os
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from . import db
 
 app = Flask(__name__)
 
 app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'tourdeflask.sqlite'),
+    # TEMPLATE_FOLDER=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lecturer')
 )
 
 # ensure the instance folder exists
@@ -19,13 +19,17 @@ db.init_app(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return "Hello TdA"
+def hello_world():
+    return render_template('home/index.html')
 
 @app.route('/api')
 def api():
-    secret = {"secret":"The cake is a lie"}
+    secret = {"secret": "The cake is a lie"}
     return jsonify(secret)
+
+@app.route('/lecturer')
+def lecturer_page():
+    return render_template('lecturer/index.html')
 
 if __name__ == '__main__':
     app.run()
